0b60b07afb398a803d72779c9ba8d5d0204bac59
tamazyanarsen@mail.ru <tamazyanarsen@mail.ru>
2024-02-12 18:00:41 +0300

    fix

--- lua/plugins/nvim.lua
+++ lua/plugins/nvim.lua
@@ -45,7 +45,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<leader>or', '<cmd>OrganizeImports<cr>', opts)
+    vim.keymap.set('n', '<space>or', '<cmd>OrganizeImports<cr>', opts)
     vim.keymap.set('n', 'fm', function()
       vim.lsp.buf.format { async = true }
     end, opts)
